bb153af6605dc4f843b7b44ce0d892efb4478dd7
Matthew <mfarmer5102@gmail.com>
2019-08-09 12:25:13 -0400

    Attempting to deploy to heroku with MongoDB

--- config/database.js
+++ config/database.js
@@ -2,10 +2,10 @@
 
 //****************************************************
 var mongo = require('mongodb');
-let databaseName = 'career_coach';
+let databaseName = process.env.DATABASE_NAME
 let entriesCollection = 'job_apps';
 var MongoClient = require('mongodb').MongoClient;
-var url = `mongodb://localhost:27017/${databaseName}`;
+var url = process.env.MONGODB_URI //`mongodb://localhost:27017/${databaseName}`;
 //****************************************************
 
 module.exports = {mongo, databaseName, entriesCollection, MongoClient, url}
