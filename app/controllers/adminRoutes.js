c0d0c7791165a0da2e5657e06dc8e0f406acb0ad
Matthew <mfarmer5102@gmail.com>
2019-08-14 16:17:08 -0400

    Cleared the admin routes to enhance security

--- app/controllers/adminRoutes.js
+++ app/controllers/adminRoutes.js
@@ -14,35 +14,5 @@ let MongoClient = dbImport.MongoClient
 let url = dbImport.url
 //****************************************************
 
-//Find all entries
-router.get('/api/show-me-everything', (req, res) => {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(databaseName);
-        dbo.collection(entriesCollection).find({}).toArray(function (err, result) {
-            if (err) throw err;
-            db.close();
-            return res.json({
-                data: result
-            })
-        });
-    })
-});
-
-//Do a bulk insert
-router.post('/insertmany', (req, res) => {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var dbo = db.db(databaseName);
-        var myobj = req.body; //Where req.body is an array of objects
-        dbo.collection(entriesCollection).insertMany(myobj, function (err, res) {
-            if (err) throw err;
-            console.log("Number of documents inserted: " + res.insertedCount);
-            db.close();
-            return res.json({})
-        });
-    });
-});
-
 module.exports = router;
 
