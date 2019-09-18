5d6c644d36a2dd4dac7e09c3cb26a7ada6ed3e09
Matthew <mfarmer5102@gmail.com>
2019-09-18 13:01:43 -0400

    Built a back-of-card view

--- src/JobCard.js
+++ src/JobCard.js
@@ -12,7 +12,8 @@ class JobCard extends Component {
     super(props);
     this.state = {
       progressPercentage: 0,
-      activeClass: "card p-2 mb-3 shadow"
+      activeClass: "card p-2 mb-3 shadow",
+      sideShowing: "front"
     };
   }
 
@@ -21,8 +22,8 @@ class JobCard extends Component {
   };
 
   calculateProgressPercentage = arg => {
-    if (arg === "Outstanding 1 or More Weeks") {
-      this.setState({ progressPercentage: 10 });
+    if (arg === "Application Submitted") {
+      this.setState({ progressPercentage: 5 });
     }
   };
 
@@ -34,62 +35,100 @@ class JobCard extends Component {
     this.setState({ activeClass: "card p-2 mb-3 shadow" });
   };
 
+  flipCard = () => {
+    if (this.state.sideShowing === "front") {
+      this.setState({ sideShowing: "back" });
+    } else {
+      this.setState({ sideShowing: "front" });
+    }
+  };
+
   render() {
     //Define progress percentage
-    let progressStyle = { width: `{this.state.progressPercentage}%` };
+    let progressStyle = { width: `${this.state.progressPercentage}%` };
 
     //Define the front side of the card
     let frontSide = (
-      <div className="col-6">
-        <div
-          className={this.state.activeClass}
-          onMouseEnter={this.inflate}
-          onMouseLeave={this.deflate}
-        >
-          <div class="card-body">
-            <span style={style.title} class="card-title">
-              {this.props.data.companyName}
-            </span>
-            <br />
-            <span class="card-text ml">
-              <small class="text-muted">
-                {this.props.data.applicationDate}
-              </small>
-            </span>{" "}
-            <span class="card-text ml">
-              <small class="text-muted">*** Days Since N/A***</small>
-            </span>
-            <p class="card-text">{this.props.data.jobTitle}</p>
-            <div className="progress">
-              <div
-                class="progress-bar"
-                role="progressbar"
-                style={progressStyle}
-              ></div>
-            </div>
-            <span class="card-text">
-              <small class="text-muted">{this.props.data.progress}</small>
-            </span>{" "}
-            |{" "}
-            <span class="card-text">
-              <small class="text-muted">{this.props.data.closure}</small>
-            </span>
+      <div
+        className={this.state.activeClass}
+        onMouseEnter={this.inflate}
+        onMouseLeave={this.deflate}
+      >
+        <div class="card-body">
+          <span style={style.title} class="card-title">
+            {this.props.data.companyName}
+          </span>
+          <br />
+          <span class="card-text ml">
+            <small class="text-muted">{this.props.data.applicationDate}</small>
+          </span>{" "}
+          <span class="card-text ml">
+            <small class="text-muted">*** Days Since N/A***</small>
+          </span>
+          <p class="card-text">{this.props.data.jobTitle}</p>
+          <div className="progress">
+            <div
+              class="progress-bar"
+              role="progressbar"
+              style={progressStyle}
+            ></div>
           </div>
+          <span class="card-text">
+            <small class="text-muted">{this.props.data.progress}</small>
+          </span>{" "}
+          |{" "}
+          <span class="card-text">
+            <small class="text-muted">{this.props.data.closure}</small>
+          </span>
         </div>
       </div>
     );
 
     //Define the back side of the card
     let backSide = (
-      <div className="col-6">
-        <div className="card p-2 mb-3 shadow">
-          <p>{this.props.data.companyName}</p>
+      <div
+        className={this.state.activeClass}
+        onMouseEnter={this.inflate}
+        onMouseLeave={this.deflate}
+      >
+        <div class="card-body">
+          <span style={style.title} class="card-title">
+            {this.props.data.companyName}, {this.props.data.jobTitle}
+          </span>
+          <br />
+          <div class="card-text ml">
+            <small class="text-muted">
+              Heard back on: {this.props.data.responseDate}
+            </small>
+          </div>
+          <div class="card-text ml">
+            <small class="text-muted">
+              Comments: {this.props.data.comments}
+            </small>
+          </div>
+          <hr></hr>
+          <div>
+            <i className="fas fa-edit btn btn-info float-left"></i>
+            <i className="fas fa-trash-alt btn btn-danger float-right"></i>
+          </div>
         </div>
       </div>
     );
 
+    //Pick side to show
+    let sideToShow;
+    if (this.state.sideShowing === "front") {
+      sideToShow = frontSide;
+    } else {
+      sideToShow = backSide;
+    }
+
     //Return
-    return frontSide;
+    return (
+      <div className="col-6" onClick={this.flipCard}>
+        {sideToShow}
+      </div>
+    );
   }
 }
 
