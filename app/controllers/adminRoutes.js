478ed63b32097b7628bd96b6ebc67ddabc73df93
Matthew <mfarmer5102@gmail.com>
2019-09-18 21:13:50 -0400

    Implemented an admin route to gather truncated data

--- app/controllers/adminRoutes.js
+++ app/controllers/adminRoutes.js
@@ -1,18 +1,48 @@
 // HANDELBARS TEMPLATE SERVING /////////////////////////////////////////////////////////////////////////
 
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-var uuid = require('uuid');
-const uuidv4 = require('uuid/v4');
+var uuid = require("uuid");
+const uuidv4 = require("uuid/v4");
 
 //****************************************************
-const dbImport = require('../config/database.js')
-let mongo = dbImport.mongo
-let databaseName = dbImport.databaseName
-let entriesCollection = dbImport.entriesCollection
-let MongoClient = dbImport.MongoClient
-let url = dbImport.url
+const dbImport = require("../config/database.js");
+let mongo = dbImport.mongo;
+let databaseName = dbImport.databaseName;
+let entriesCollection = dbImport.entriesCollection;
+let MongoClient = dbImport.MongoClient;
+let url = dbImport.url;
 //****************************************************
+
+router.get("/touslesrecords", (req, res) => {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db(databaseName);
+    dbo
+      .collection(entriesCollection)
+      .find({})
+      .sort({ applicationSubmissionDate: -1 })
+      .toArray(function(err, result) {
+        if (err) throw err;
+        db.close();
+        var modifiedResponseArray = [];
+        for (i = 0; i < result.length; i++) {
+          let modObj = {};
+          modObj.companyName = result[i].companyName;
+          modObj.jobTitle = result[i].jobTitle;
+          modObj.applicationDate = result[i].applicationSubmissionDate;
+          modObj.firstResponseDate = result[i].dateOfFirstResponse;
+          modObj.isMajorCorporation = result[i].majorCorporation;
+          modObj.progress = result[i].progress;
+          modObj.closure = result[i].close;
+          modObj.comments = result[i].comments;
+          modifiedResponseArray.push(modObj);
+        }
+        return res.json({
+          data: modifiedResponseArray
+        });
+      });
+  });
+});
 
 module.exports = router;
-
